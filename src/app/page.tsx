b01383a4e9c211cfdb1fbec29e1dfc65406848b5
rlawo <rlawo32@naver.com>
2024-04-10 16:35:07 +0900

    test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import styles from "./page.module.css";
 export default function Home() {
   return (
     <main className={styles.main}>
-      <h1>hello!!!</h1>
+      <h1>hello!</h1>
     </main>
   );
 }

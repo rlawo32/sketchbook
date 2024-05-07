f9a19adece543ed572aca6b9d3105d680e038b5f
sungjae <92013056+rlawo32@users.noreply.github.com>
2024-05-07 17:53:58 +0900

    Update and rename apiTest.ts to apiTest.tsx

--- src/app/process/apiTest.ts
+++
@@ -1,7 +0,0 @@
-import { NextResponse } from "next/server";
-
-// app 폴더 위에 api 폴더 생성 후 원하는 api용 폴더 이름 생성, route.ts 파일로 api 소스 작성
-
-export async function POST(request: Request) {
-    return NextResponse.json({ message: 'Hello from Next.js!' })
-}

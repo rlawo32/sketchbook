477128d0326c0064219f301e0c36c7cb871caecf
sungjae <92013056+rlawo32@users.noreply.github.com>
2024-05-17 13:28:57 +0900

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -20,9 +20,9 @@ export default function RootLayout({
     <html lang="ko">
       <body className={inter.className}>
         <StyledComponentsRegistry>
-          {/* <JotaiProviderWrapper> */}
+          <JotaiProviderWrapper>
             {children}
-          {/* </JotaiProviderWrapper> */}
+          </JotaiProviderWrapper>
         </StyledComponentsRegistry>
       </body>
     </html>

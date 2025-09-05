9cb74b4d9e5df5d2b958ccd529735765b8e1baaa
sungjae <92013056+rlawo32@users.noreply.github.com>
2025-09-05 17:39:10 +0900

    Create page.style.tsx

---
+++ src/app/reference/dnd/page.style.tsx
@@ -0,0 +1,16 @@
+import styled from "styled-components";
+
+export const DndStyle = styled('div')`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+
+    .list_section {
+        display: flex;
+
+        .list_wrap {
+            margin: 0 10px;
+
+        }
+    }
+`

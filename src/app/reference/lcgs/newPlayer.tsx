a0d4bc549fcdb8cf27443d9c7a25a0750b3f1a14
sungjae <92013056+rlawo32@users.noreply.github.com>
2026-08-25 17:58:25 +0900

    Refactor player selection to use SelectBoxRelativeV2

--- src/app/reference/lcgs/newPlayer.tsx
+++ src/app/reference/lcgs/newPlayer.tsx
@@ -2,10 +2,48 @@
 
 import * as Style from "./match_player.style";
 
+import TopIcon from "../icons/TopIcon";
+import JugIcon from "../icons/JugIcon";
+import MidIcon from "../icons/MidIcon";
+import AdcIcon from "../icons/AdcIcon";
+import SupIcon from "../icons/SupIcon";
+import SelectBoxRelativeV2 from "./select_box_relative_v2";
+import { useState } from "react";
+
 const MatchPlayer = () => {
+
+    const [selectOpponent, setSelectOpponent] = useState<string>("");
 
     const laneArr:string[] = ["ALL", "TOP", "JUG", "MID", "ADC", "SUP"];
     const resultArr:string[] = ["TEST1", "TEST2", "TEST3"];
+    const tempArr:{
+            lcg_summoner_puuid: string 
+            lcg_player: string 
+            lcg_summoner_name: string 
+            lcg_summoner_nickname: string
+            lcg_player_hide: string}[] = [
+        {lcg_summoner_puuid:'1e062cfe-c62e-53ef-9145-ab0d6c76d40d', lcg_player:'성재', lcg_summoner_name:'버들보들', lcg_summoner_nickname:'버들보들', lcg_player_hide:'N'},
+        {lcg_summoner_puuid:'fd234707-5d0b-5db9-92e1-9b8fae3b1b84', lcg_player:'지훈', lcg_summoner_name:'까까라돈나', lcg_summoner_nickname:'까까라돈나', lcg_player_hide:'N'},
+        {lcg_summoner_puuid:'60e3571d-2b64-5e2b-b9ba-c73789b86639', lcg_player:'광호', lcg_summoner_name:'윤슬l', lcg_summoner_nickname:'윤슬l', lcg_player_hide:'N'},
+        {lcg_summoner_puuid:'fd234707-5d0b-5db9-92e1-9b8fae3b1b84', lcg_player:'지훈', lcg_summoner_name:'까까라돈나', lcg_summoner_nickname:'까까라돈나', lcg_player_hide:'N'},
+        {lcg_summoner_puuid:'60e3571d-2b64-5e2b-b9ba-c73789b86639', lcg_player:'광호', lcg_summoner_name:'윤슬l', lcg_summoner_nickname:'윤슬l', lcg_player_hide:'N'},
+        {lcg_summoner_puuid:'fd234707-5d0b-5db9-92e1-9b8fae3b1b84', lcg_player:'지훈', lcg_summoner_name:'까까라돈나', lcg_summoner_nickname:'까까라돈나', lcg_player_hide:'N'},
+        {lcg_summoner_puuid:'60e3571d-2b64-5e2b-b9ba-c73789b86639', lcg_player:'광호', lcg_summoner_name:'윤슬l', lcg_summoner_nickname:'윤슬l', lcg_player_hide:'N'},
+        {lcg_summoner_puuid:'fd234707-5d0b-5db9-92e1-9b8fae3b1b84', lcg_player:'지훈', lcg_summoner_name:'까까라돈나', lcg_summoner_nickname:'까까라돈나', lcg_player_hide:'N'},
+        {lcg_summoner_puuid:'60e3571d-2b64-5e2b-b9ba-c73789b86639', lcg_player:'광호', lcg_summoner_name:'윤슬l', lcg_summoner_nickname:'윤슬l', lcg_player_hide:'N'},
+        {lcg_summoner_puuid:'fd234707-5d0b-5db9-92e1-9b8fae3b1b84', lcg_player:'지훈', lcg_summoner_name:'까까라돈나', lcg_summoner_nickname:'까까라돈나', lcg_player_hide:'N'},
+        {lcg_summoner_puuid:'60e3571d-2b64-5e2b-b9ba-c73789b86639', lcg_player:'광호', lcg_summoner_name:'윤슬l', lcg_summoner_nickname:'윤슬l', lcg_player_hide:'N'},
+        {lcg_summoner_puuid:'fd234707-5d0b-5db9-92e1-9b8fae3b1b84', lcg_player:'지훈', lcg_summoner_name:'까까라돈나', lcg_summoner_nickname:'까까라돈나', lcg_player_hide:'N'},
+        {lcg_summoner_puuid:'60e3571d-2b64-5e2b-b9ba-c73789b86639', lcg_player:'광호', lcg_summoner_name:'윤슬l', lcg_summoner_nickname:'윤슬l', lcg_player_hide:'N'},
+        {lcg_summoner_puuid:'fd234707-5d0b-5db9-92e1-9b8fae3b1b84', lcg_player:'지훈', lcg_summoner_name:'까까라돈나', lcg_summoner_nickname:'까까라돈나', lcg_player_hide:'N'},
+        {lcg_summoner_puuid:'60e3571d-2b64-5e2b-b9ba-c73789b86639', lcg_player:'광호', lcg_summoner_name:'윤슬l', lcg_summoner_nickname:'윤슬l', lcg_player_hide:'N'},
+        {lcg_summoner_puuid:'fd234707-5d0b-5db9-92e1-9b8fae3b1b84', lcg_player:'지훈', lcg_summoner_name:'까까라돈나', lcg_summoner_nickname:'까까라돈나', lcg_player_hide:'N'},
+        {lcg_summoner_puuid:'60e3571d-2b64-5e2b-b9ba-c73789b86639', lcg_player:'광호', lcg_summoner_name:'윤슬l', lcg_summoner_nickname:'윤슬l', lcg_player_hide:'N'},
+        {lcg_summoner_puuid:'fd234707-5d0b-5db9-92e1-9b8fae3b1b84', lcg_player:'지훈', lcg_summoner_name:'까까라돈나', lcg_summoner_nickname:'까까라돈나', lcg_player_hide:'N'},
+        {lcg_summoner_puuid:'60e3571d-2b64-5e2b-b9ba-c73789b86639', lcg_player:'광호', lcg_summoner_name:'윤슬l', lcg_summoner_nickname:'윤슬l', lcg_player_hide:'N'},
+        {lcg_summoner_puuid:'fd234707-5d0b-5db9-92e1-9b8fae3b1b84', lcg_player:'지훈', lcg_summoner_name:'까까라돈나', lcg_summoner_nickname:'까까라돈나', lcg_player_hide:'N'},
+        {lcg_summoner_puuid:'60e3571d-2b64-5e2b-b9ba-c73789b86639', lcg_player:'광호', lcg_summoner_name:'윤슬l', lcg_summoner_nickname:'윤슬l', lcg_player_hide:'N'},
+    ];
     const selectOption:string = "A"; // 기본(All) - A, 상대(Opponent) - O, 라인(Lane) - L, 상대&라인(Merge) - M
 
     return (
@@ -47,11 +85,7 @@ const MatchPlayer = () => {
                                 })}
                             </div>
                             <div className="relative_select_player">
-                                <select>
-                                    <option>성재</option>
-                                    <option>지훈</option>
-                                    <option>광호</option>
-                                </select>
+                                <SelectBoxRelativeV2 playerList={tempArr} selectOpponent={selectOpponent} setSelectOpponent={setSelectOpponent}/>
                             </div>
                         </div>
                         <div className="relative_body">
